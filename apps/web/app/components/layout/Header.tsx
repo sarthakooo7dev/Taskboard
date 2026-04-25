4644cbdc872f19e7549b351a20b573f868c1afd8
Sarthak Shukla <sarthak.shukla007@gmail.com>
2026-04-25 13:59:09 +0530

    feat(header):fix layout

--- apps/web/app/components/layout/Header.tsx
+++ apps/web/app/components/layout/Header.tsx
@@ -1,8 +1,28 @@
+"use client"
+
+import { usePathname } from 'next/navigation'
 import React from 'react'
+import { SIDEBAR_CONFIG } from '../../config/sidebar.config'
 
 const Header = () => {
+
+    const path = usePathname()
+    const headerInfo = SIDEBAR_CONFIG[0]?.items.find((val) => val.href === path);
+    const title = headerInfo?.label;
+    const info = headerInfo?.info;
+
     return (
-        <div className='border-b border-b-dk_border  h-[4.6rem] bg-dk_grey'>header</div>
+        <div className='flex justify-center border-b border-b-dk_border p-2 h-[4rem] bg-dk_grey'>
+
+            <div className='flex-1 '>
+                <h2 className='text-lg font-semibold opacity-90 tracking-[1px] '>{title}</h2>
+                <p className='text-sm text-gray-400'>{info}</p>
+            </div>
+            <div className='bd_grn w-60'>
+
+            </div>
+
+        </div>
     )
 }
 
--- apps/web/app/config/sidebar.config.ts
+++ apps/web/app/config/sidebar.config.ts
@@ -11,24 +11,28 @@ export const SIDEBAR_CONFIG = [
         label: 'Overview',
         icon: LayoutDashboard,
         href: ROUTES.dashboard,
+        info:"Monitor all your boards and tasks here"
       },
       {
         id: 'boards',
         label: 'Boards',
         icon: Presentation,
         href: ROUTES.boards,
+        info:"boards"
       },
       {
         id: 'tasks',
         label: 'Tasks',
         icon: CheckSquare,
         href: ROUTES.tasks,
+        info:"tasks"
       },
       {
         id: 'members',
         label: 'Members',
         icon: Users,
         href: ROUTES.members,
+        info:"members"
       },
     ],
   },
