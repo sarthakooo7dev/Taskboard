e905e9e52dbe45343686fbee5d5179ce6ffb2dd1
Sarthak Shukla <sarthak.shukla007@gmail.com>
2026-05-03 16:04:54 +0530

    feat(Board page DB): designed api for board summary.

---
+++ apps/web/app/api/boards/summary/route.ts
@@ -0,0 +1,121 @@
+import { checkAuthorization } from '@/app/lib/validators/user.validator'
+import { prisma } from '@repo/db'
+import { NextResponse } from 'next/server'
+
+import z, { ZodError } from 'zod'
+
+// Get all boards for the user
+export async function GET() {
+  try {
+    const session = await checkAuthorization()
+    const BoardData = await prisma.boardMember.findMany({
+      where: {
+        //@ts-ignore
+        userId: session.user.id,
+      },
+      select: {
+        boardId: true,
+        role: true,
+
+        board: {
+          select: {
+            id: true,
+            name: true,
+            Description: true,
+            updatedAt: true,
+
+            _count: {
+              select: {
+                members: true,
+              },
+            },
+
+            // ✅ NOW SAFE TO LIMIT
+            members: {
+              take: 3,
+              orderBy: {
+                joinedAt: 'asc',
+              },
+              select: {
+                user: {
+                  select: {
+                    id: true,
+                    name: true,
+                    avatar: true,
+                  },
+                },
+              },
+            },
+
+            columns: {
+              select: {
+                type: true,
+                _count: {
+                  select: { tasks: true },
+                },
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        board: {
+          updatedAt: 'desc',
+        },
+      },
+    })
+
+    const boardSummaryData = BoardData.map(({ board, role, boardId }) => {
+      let totalTasks = 0
+      let blockedTasks = 0
+      let inProgress = 0
+
+      for (const col of board.columns) {
+        const count = col._count.tasks
+
+        totalTasks += count
+
+        if (col.type === 'BLOCKED') blockedTasks += count
+        if (col.type === 'IN_PROGRESS') inProgress += count
+      }
+
+      return {
+        boardId,
+        title: board.name,
+        role,
+        description: board.Description,
+        updatedAt: board.updatedAt,
+        totalMembers: board._count.members,
+        members: board.members.map((m) => m.user),
+        totalTasks,
+        blockedTasks,
+        inProgress,
+      }
+    })
+
+    return NextResponse.json({
+      data: boardSummaryData,
+    })
+  } catch (err) {
+    if (err instanceof Error && err.message == 'UNAUTHORIZED') {
+      return NextResponse.json(
+        {
+          error: {
+            code: 'UNAUTHORIZED',
+            message: 'You must be logged in !',
+          },
+        },
+        { status: 401 },
+      )
+    }
+    return NextResponse.json(
+      {
+        error: {
+          code: 'INTERNAL_ERROR',
+          message: 'Something went wrong',
+        },
+      },
+      { status: 500 },
+    )
+  }
+}
--- apps/web/app/dashboard/boards/page.tsx
+++ apps/web/app/dashboard/boards/page.tsx
@@ -1,34 +1,14 @@
+"use client"
 import BoardCard from '@/app/components/utility/board-utils/BoardCard'
-import React from 'react'
+import { BoardCardProps } from '@/app/types/general.types';
+import React, { useState, useEffect } from 'react'
 
 const page = () => {
 
-    const mem = [
-        {
-            id: "1",
-            name: "Sanjay",
-            image: "/avatars/avatar1.png",
-        },
-        {
-            id: "2",
-            name: "Rahul",
-            image: "/avatars/avatar2.png",
-        },
-        {
-            id: "3",
-            name: "Aman",
-            image: "/avatars/avatar3.png",
-        },
-        {
-            id: "4",
-            name: "Priya",
-            image: "/avatars/avatar4.png",
-        },
-    ]
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 p-3 ">
-            <BoardCard
+            {/* <BoardCard
                 title="Product Roadmap"
                 boardId="c58596f3-5c39-49e1-bdb6-fe4cc05470b8"
                 role="MANAGER"
@@ -38,19 +18,8 @@ const page = () => {
                 inProgress={3}
                 updatedAt="2h ago"
                 members={mem}
-            />
+            /> */}
 
-            <BoardCard
-                title="Design & Development"
-                boardId="aqqqqqqqqqqqqqq ewredgvfdfg"
-                role="MANAGER"
-                description="Coordinating design and development tasks to ensure smooth delivery"
-                totalTasks={12}
-                blockedTasks={0}
-                inProgress={3}
-                updatedAt="2h ago"
-                members={mem}
-            />
         </div>
     )
 }
