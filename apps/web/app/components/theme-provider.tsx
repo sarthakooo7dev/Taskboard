8c2201127ca4f7c111503945e47f079bcc52ed1e
Sarthak Shukla <sarthak.shukla007@gmail.com>
2026-08-14 13:57:50 +0530

    fix v1.0.3 : default theme fixed.

--- apps/web/app/components/theme-provider.tsx
+++ apps/web/app/components/theme-provider.tsx
@@ -1,15 +1,15 @@
-"use client"
+'use client'
 
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-    return (
-        <NextThemesProvider
-            attribute="class"
-            defaultTheme="light"
-            enableSystem={false}
-        >
-            {children}
-        </NextThemesProvider>
-    )
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
